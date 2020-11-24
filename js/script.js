1624404d4caf999d8a0130441b03ccaedcdaeaaa
Zach Kessler <zachnkessler@gmail.com>
2020-11-24 15:09:21 -0600

    Step 4 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 let userName = document.querySelector("#name").focus();
+
+let jobRole = document.querySelector("#title");
+let otherJobRole = document.querySelector(".other-job-role");
+
+otherJobRole.style.display = "none";
+
+jobRole.addEventListener("change", (e) => {
+    let other = "OTHER";
+    if (e.target.value === other.toLowerCase()) {
+        otherJobRole.style.display = "";
+    } else {
+        otherJobRole.style.display = "none";
+    }
+})
