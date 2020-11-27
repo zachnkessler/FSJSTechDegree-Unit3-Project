ca8c25cf99aef2ca17814ff8c014b434130b9371
Zach Kessler <zachnkessler@gmail.com>
2020-11-27 13:39:33 -0600

    comments added

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 Treehouse FSJS Techdegree:
 Project 3 - Interactive Form
 Zachary Kessler
-11/25/2020
+11/27/2020
 ******************************************/
 
+// creates variable to store and place focus on name input element
 let userName = document.querySelector("#name");
 userName.focus();
+
+// creates event listener to display a text input field when job selection is "other"
 let jobRole = document.querySelector("#title");
 let otherJobRole = document.querySelector(".other-job-role");
 
@@ -23,6 +26,7 @@ jobRole.addEventListener("change", (e) => {
 
 ////////////////////////////////////////
 
+// creates an event listener to display only the applicable t-shirt colors upon selection of a t-shirt design
 let design = document.querySelector("#design");
 let color = document.querySelector("#color");
 let colorChildren = color.children;
@@ -44,6 +48,7 @@ design.addEventListener("change", (e) => {
 
 ////////////////////////////////////////
 
+// creates an event listener to calculate and display a running total for the activity costs
 let activities = document.querySelector("#activities");
 let totalActivitiesCost = document.querySelector("#activities-cost");
 
@@ -62,6 +67,7 @@ activities.addEventListener("change", (e) => {
 
 ////////////////////////////////////////
 
+// creates an event listener to display and hide the applicable elements associated with the relevant payment type
 let payment = document.querySelector("#payment");
 let creditCard = document.querySelector("#credit-card");
 let paypal = document.querySelector("#paypal");
@@ -91,6 +97,11 @@ payment.addEventListener("change", (e) => {
 
 ////////////////////////////////////////
 
+/* creates an event listener to test various input values and prevent the form 
+from submitting if there is an error. Also displays or hides error messages indicating
+what needs to be corrected when an input's value does not pass the regex test.
+*/ 
+
 let userEmail = document.querySelector("#email");
 let creditCardNumber = document.querySelector("#cc-num");
 let userZipCode = document.querySelector("#zip");
@@ -98,34 +109,117 @@ let CVV = document.querySelector("#cvv");
 let form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
+    // name validation
     let nameValue = userName.value;
     let nameTest = /^[a-zA-Z]+$/i.test(nameValue);
+    let nameParent = userName.parentNode;
+    let nameHint = document.querySelector("#name-hint");
     if (nameTest === false) {
         e.preventDefault();
-    };
+        nameParent.classList.add("not-valid");
+        nameParent.classList.remove("valid");
+        nameHint.style.display = "block";
+    } else {
+        nameParent.classList.add("valid");
+        nameParent.classList.remove("not-valid");
+        nameHint.style.display = "none";
+    }
+    // email validation
     let emailValue = userEmail.value;
+    // regex code sourced from https://www.w3resource.com/javascript/form/email-validation.php on 11/25/2020
     let emailTest = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/i.test(emailValue);
+    let emailParent = userEmail.parentNode;
+    let emailHint = document.querySelector("#email-hint");
     if (emailTest === false) {
         e.preventDefault();
+        emailParent.classList.add("not-valid");
+        emailParent.classList.remove("valid");
+        emailHint.style.display = "block";
+    } else {
+        emailParent.classList.add("valid");
+        emailParent.classList.remove("not-valid");
+        emailHint.style.display = "none";
     }
-
-    // Activity Selection Validation goes here
-
+    // activity selection validation
+    let checkboxes = document.querySelectorAll("[type='checkbox']");
+    let numberChecked = 0;
+    let activitiesBox = document.querySelector("#activities");
+    let activitiesHint = document.querySelector("#activities-hint");
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked === true) {
+            numberChecked += 1;
+        }
+    }
+    if (numberChecked === 0) {
+        e.preventDefault();
+        activitiesBox.classList.add("not-valid");
+        activitiesBox.classList.remove("valid");
+        activitiesHint.style.display = "block";
+    } else {
+        activitiesBox.classList.add("valid");
+        activitiesBox.classList.remove("not-valid");
+        activitiesHint.style.display = "none";
+    }
+    // payment validation 
     if (payment.children[1].selected === true) {
         let creditCardValue = creditCardNumber.value;
-        let creditCardTest = /^\d{4}-\d{4}-\d{4}-\d{4}$/.test(creditCardValue);
+        let creditCardTest = /^\d{13,16}$/.test(creditCardValue);
+        let creditCardParent = creditCard.parentNode;
+        let creditCardHint = document.querySelector("#cc-hint");
         if (creditCardTest === false) {
             e.preventDefault();
+            creditCardParent.classList.add("not-valid");
+            creditCardParent.classList.remove("valid");
+            creditCardHint.style.display = "block";
+        } else {
+            creditCardParent.classList.add("valid");
+            creditCardParent.classList.remove("not-valid");
+            creditCardHint.style.display = "none";
         }
         let zipCodeValue = userZipCode.value;
         let zipCodeTest = /^\d{5}$/.test(zipCodeValue);
+        let zipCodeParent = userZipCode.parentNode;
+        let zipCodeHint = document.querySelector("#zip-hint");
         if (zipCodeTest === false) {
             e.preventDefault();
+            zipCodeParent.classList.add("not-valid");
+            zipCodeParent.classList.remove("valid");
+            zipCodeHint.style.display = "block";
+        } else {
+            zipCodeParent.classList.add("valid");
+            zipCodeParent.classList.remove("not-valid");
+            zipCodeHint.style.display = "none";
         }
         let CVVValue = CVV.value;
-        let CVVTest = /^\d{3,4}$/.test(CVVValue);
+        let CVVTest = /^\d{3}$/.test(CVVValue);
+        let CVVParent = CVV.parentNode;
+        let CVVHint = document.querySelector("#cvv-hint");
         if (CVVTest === false) {
             e.preventDefault();
+            CVVParent.classList.add("not-valid");
+            CVVParent.classList.remove("valid");
+            CVVHint.style.display = "block";
+        } else {
+            CVVParent.classList.add("valid");
+            CVVParent.classList.remove("not-valid");
+            CVVHint.style.display = "none";
         }
     }
 })
+
+////////////////////////////////////////
+
+// creates a loop to add additional styles when selecting activities
+let checkboxes = document.querySelectorAll("[type='checkbox']");
+
+for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("focus", () => {
+        let checkParent = checkboxes[i].parentNode;
+        checkParent.classList.add("focus");
+    })
+    checkboxes[i].addEventListener("blur", () => {
+        let checkParent = checkboxes[i].parentNode;
+        checkParent.classList.remove("focus");
+    })
+}
+
